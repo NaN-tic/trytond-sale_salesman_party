b653bc354bad78e44571afc3c2d1998e9b413b51
?ngel ?lvarez <angel@nan-tic.com>
2014-07-03 10:20:43 +0200

    initial commit

---
+++ sale.py
@@ -0,0 +1,45 @@
+# This file is part of sale_salesman module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from trytond.model import ModelSQL, fields
+from trytond.pool import PoolMeta
+
+__all__ = ['PartyEmployee', 'Party', 'Employee', 'Sale']
+__metaclass__ = PoolMeta
+
+
+class PartyEmployee(ModelSQL):
+    'Party - Employee'
+    __name__ = 'party.party-company.employee'
+    party = fields.Many2One('party.party', 'Party', required=True,
+        select=True, ondelete='CASCADE')
+    employee = fields.Many2One('company.employee', 'Salesman', required=True,
+        select=True, ondelete='CASCADE')
+
+
+class Party:
+    __name__ = 'party.party'
+
+    salesmans = fields.Many2Many('party.party-company.employee', 'party',
+        'employee', 'Salesmans')
+
+
+class Employee:
+    __name__ = 'company.employee'
+
+    sale_parties = fields.Many2Many('party.party-company.employee', 'employee',
+        'party', 'Sale Parties')
+
+
+class Sale:
+    __name__ = 'sale.sale'
+
+    @fields.depends('party')
+    def on_change_party(self):
+        res = super(Sale, self).on_change_party()
+        if self.party:
+            if len(self.party.salesmans) == 1:
+                res['employee'] = self.party.salesmans[0].id
+            else:
+                res['employee'] = None
+        return res
